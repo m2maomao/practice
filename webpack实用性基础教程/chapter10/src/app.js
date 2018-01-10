d95a1b61b8dc9a28cb436a48633db9a7220b77df
张国龙 <m2maomao@163.com>
2018-01-10 22:50:10 +0800

    add 10 chapter code

--- webpack实用性基础教程/chapter10/src/app.js
+++ webpack实用性基础教程/chapter10/src/app.js
@@ -2,21 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './common/style/main.css';
-import './common/style/other.css';
-
-import dog from './common/img/avatar.png';
-import a from './common/img/a.jpg';
-const b = require('./common/img/b.jpg');
-
-
-console.log(dog);
 
 ReactDOM.render(
-    <div className="blue_background">
-        <img src={dog} />
-        <img src={a} />
-        <img src={b} />
-        <img src={require('./common/img/c.jpg')} />
-    </div>, 
+    <div className="star">React</div>, 
     document.getElementById('root')
 ) 
--- webpack实用性基础教程/chapter10/webpack.config.dev.js
+++ webpack实用性基础教程/chapter10/webpack.config.dev.js
@@ -27,10 +27,14 @@ module.exports = {
                     }
                 ]
             },
+            //处理css文件中出现的url，会自动帮你引入里面要引入的模块
             {
                 test:/\.css$/,
                 use:['style-loader','css-loader']
             },
+            // file-loader
+            // 1.把你的资源移动到输出目录
+            // 2.返回最终引入资源的url
             {
                 test:/\.(jpg|png|gif|jpeg)$/,
                 use:[{
@@ -39,6 +43,10 @@ module.exports = {
                         limit:10000
                     }
                 }]
+            },
+            {
+                test:/\.(svg|ttf|eot|svg|woff|woff2)$/,
+                use:['file-loader']
             }
         ]
     },
