ba85ad6ec3815734713a4b66e50786eb5d2a6966
Michael <m2maomao@163.com>
2018-07-31 14:10:43 +0800

    add axios demo

---
+++ Vue/axios/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    axios案例:<br /><br />
+    <button type="button" @click="doRequest">发送请求</button>
+    <br /><br />
+    <ul>
+      <li>{{list}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data(){
+      return{
+        list:undefined
+      }
+    },
+    methods: {
+      doRequest(){
+        var url = 'https://www.easy-mock.com/mock/5b5889ab79a0b71429dea33a/test/example';
+        this.$axios.get(url).then((response)=>{
+          console.log(response.data)
+          this.list=response.data.address.city
+          console.log(this.list)
+        }).catch(error=>{
+          console.log('发送失败')
+          console.log(error)
+        })
+      }
+    }
+  }
+</script>
