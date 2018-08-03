6a29525a88513fdaf1964aadab350c17f7ed2b16
Michael <m2maomao@163.com>
2018-08-04 00:19:00 +0800

    add header

--- Vue/去哪网App实战/7/src/pages/home/Home.vue
+++ Vue/去哪网App实战/7/src/pages/home/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <!-- <div class="home">Home</div>
-    <router-link to="/list" class="home">列表页</router-link> -->
-    hello world
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
---
+++ Vue/去哪网App实战/7/src/pages/home/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
+    <div class="header-right">城市<span class="iconfont arrow-icon">&#xe6aa;</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.header {
+  height: 0.86rem;
+  display: flex;
+  background: $bgColor;
+  color: #fff;
+  line-height: 0.86rem;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    height: 0.62rem;
+    line-height: 0.62rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    background: #ffffff;
+    border-radius: 0.1rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+
+    .arrow-icon {
+      font-size: 0.24rem;
+    }
+  }
+}
+</style>
