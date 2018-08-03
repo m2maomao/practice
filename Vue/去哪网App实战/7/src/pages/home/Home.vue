35bc2b2ccb3e1e9361052e67ca2596fcffa061c9
Michael <m2maomao@163.com>
2018-08-03 22:54:16 +0800

    7 chapter

---
+++ Vue/去哪网App实战/7/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- <div class="home">Home</div>
+    <router-link to="/list" class="home">列表页</router-link> -->
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+</style>
