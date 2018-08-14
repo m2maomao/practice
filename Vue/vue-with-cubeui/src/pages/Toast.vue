aae32447381707adbdbf819a44046eeba564932b
Michael <m2maomao@163.com>
2018-08-14 23:08:18 +0800

    change toast

--- Vue/vue-with-cubeui/src/pages/Toast.vue
+++ Vue/vue-with-cubeui/src/pages/Toast.vue
@@ -4,6 +4,8 @@
       <cube-button @click="showToastTime">Toast - time 1s</cube-button>
       <cube-button @click="showToastTime0">Toast - time 0s</cube-button>
       <cube-button @click="showToastMask">Toast - with mask</cube-button>
+      <cube-button @click="showToastType">Toast - type</cube-button>
+      <cube-button @click="showToastCallback">Toast -callback</cube-button>
     </form>
   </div>
 </template>
@@ -36,11 +38,44 @@ export default {
         mask: true
       })
       toast.show()
+    },
+    // 显示type
+    showToastType () {
+      const toast = this.$createToast({
+        type: 'correct',
+        txt: 'Correct'
+      })
+      toast.show()
+    },
+    // 显示回调
+    // showToastCallback () {
+    //   const toast = this.$createToast({
+    //     txt: 'Timeout',
+    //     time: 1000,
+    //     onTimeout: () => {
+    //       console.log('Timeout')
+    //     }
+    //   })
+    //   toast.show()
+    // },
+    // $event传入事件回调
+    showToastCallback () {
+      const toast = this.$createToast({
+        txt: 'Timeout',
+        time: 1000,
+        $events: {
+          timeout: () => {
+            console.log('Timeout')
+          }
+        }
+      })
+      toast.show()
     }
   }
 }
 </script>
 
-<style lang="stylus">
-
+<style lang="stylus" scoped>
+.cube-btn
+  border 10px solid #ffffff
 </style>
