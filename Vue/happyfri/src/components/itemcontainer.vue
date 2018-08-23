8fdaddebb2095ecde7a0e9d6a7535489dcdf939d
Michael <m2maomao@163.com>
2018-08-23 23:55:46 +0800

    add code

--- Vue/happyfri/src/components/itemcontainer.vue
+++ Vue/happyfri/src/components/itemcontainer.vue
@@ -16,13 +16,15 @@
         <div class="item_list_container" v-if="itemDetail.length > 0">
           <header class="item_title">{{itemDetail[itemNum-1].topic_name}}</header>
           <ul>
-            <li v-for="(item,index) of itemDetail[itemNum-1].topic_answer" @click="choosed(index,item.topic_answer_id)" class="item_list" :key="item.index">
+            <li v-for="(item,index) of itemDetail[itemNum-1].topic_answer" @click="choosed(index,item.topic_answer_id)" class="item_list" :key="index">
               <span class="option_style" v-bind:class="{'has_choosed':choosedNum==index}">{{chooseType(index)}}</span>
               <span class="option_detail">{{item.answer_name}}</span>
             </li>
           </ul>
         </div>
       </div>
+      <span class="next_item button_style" @click="nextItem" v-if="itemNum < itemDetail.length"></span>
+      <span class="submit_item button_style" v-else @click="submitAnswer"></span>
     </div>
   </section>
 </template>
@@ -34,7 +36,7 @@ export default {
   data () {
     return {
       itemId: null, // 题目ID
-      chooseNum: null, // 选中的答案索引
+      choosedNum: null, // 选中的答案索引
       chooseId: null// 选中答案ID
     }
   },
@@ -50,8 +52,9 @@ export default {
     // 下一题
     nextItem () {
       if (this.choosedNum !== null) {
-        this.chooseNum = null
+        this.choosedNum = null
         // 保存答案，题目索引加一，跳到下一题
+        this.addNum(this.choosedId)
       } else {
         alert('您还没有选择答案哦')
       }
@@ -59,15 +62,15 @@ export default {
     // 索引0-3对应答案A-B
     chooseType: type => {
       switch (type) {
-        case 0:return 'A'
-        case 1:return 'B'
-        case 2:return 'C'
-        case 3:return 'D'
+        case 0: return 'A'
+        case 1: return 'B'
+        case 2: return 'C'
+        case 3: return 'D'
       }
     },
     // 选中的答案信息
     choosed (type, id) {
-      this.chooseNum = type
+      this.choosedNum = type
       this.choosedId = id
     },
     // 到达最后一题，交卷，清空定时器，跳转分数页面
@@ -89,3 +92,92 @@ export default {
   }
 }
 </script>
+<style lang="stylus">
+.top_tips
+  position absolute
+  height 7.35rem
+  width 3.25rem
+  top -1.3rem
+  right 1.6rem
+  background url('../images/WechatIMG2.png') no-repeat
+  background-size 100% 100%
+  z-index 10
+  .num_tip
+    position absolute
+    left 0.48rem
+    bottom 1.1rem
+    height 0.7rem
+    width 2.5rem
+    font-size 0.6rem
+    font-family '黑体'
+    font-weight 600
+    color #a57c50
+    text-align center
+.item_container_style
+  height 11.625rem
+  width 13.15rem
+  background-repeat no-repeat
+  position absolute
+  top 4.1rem
+  left 1rem
+.home_logo
+  background-image url('../images/1-2.png')
+  background-size 13.142rem 100%
+  background-position right center
+.item_back
+  background-image url('../images/2-1.png')
+  background-size 100% 100%
+.button_style
+  display block
+  height 2.1rem
+  width 4.35rem
+  background-size 100% 100%
+  position absolute
+  top 16.5rem
+  left 50%
+  margin-left -2.4rem
+  background-repeat no-repeat
+.start
+  background-image url('../images/1-4.png')
+.next_item
+  background-image url('../images/2-2.png')
+.submit_item
+  background-image url('../images/3-1.png')
+.item_list_container
+  position absolute
+  height 7rem
+  width 8rem
+  top 2.4rem
+  left 3rem
+  -webkit-font-smoothing antialiased
+.item_title
+  font-size 0.65rem
+  color #fff
+  line-height 0.7rem
+.item_list
+  font-size 0
+  margin-top 0.4rem
+  width 10rem
+  span
+    display inline-block
+    font-size 0.6rem
+    color #fff
+    vertical-align middle
+  .option_style
+    height 0.725rem
+    width 0.725rem
+    border 1px solid #fff
+    border-radius 50%
+    line-height 0.725rem
+    text-align center
+    margin-right 0.3rem
+    font-size 0.5rem
+    font-family 'Arial'
+  .has_choosed
+    background-color #ffd400
+    color #575757
+    border-color #ffd400
+  .option_detail
+    width 7.5rem
+    padding-top 0.11rem
+</style>
--- Vue/happyfri/src/page/item/index.vue
+++ Vue/happyfri/src/page/item/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <itemcontainer father-component="item"></itemcontainer>
+  </div>
 </template>
 
 <script>
+import itemcontainer from '@/components/itemcontainer'
 export default {
-
+  name: 'item',
+  components: {
+    itemcontainer
+  },
+  created () {
+    this.$store.commit('REMEMBER_TIME')
+  }
 }
 </script>
