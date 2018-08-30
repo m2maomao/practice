429d45f5d04b0629fd3f7efb64ddcedf009bfe7f
Michael <m2maomao@163.com>
2018-08-30 17:55:49 +0800

    add code

--- ES6/ES6TEST/src/index.js
+++ ES6/ES6TEST/src/index.js
@@ -67,31 +67,59 @@
 //     return value > 5;
 // }))
 
-var json1 = '{"name":"GDT","age":"23","University":"GDUT"}'
-var json2 = {"name":"GDT","age":"23","University":"GDUT"}
+// var json1 = '{"name":"GDT","age":"23","University":"GDUT"}'
+// var json2 = {"name":"GDT","age":"23","University":"GDUT"}
 
-console.log('json1 type:',typeof(json1),json1) //json1 type:string {"name":"GDT","age":"23","University":"GDUT"}
-console.log('json2 type:',typeof(json2),json2) //json2 type:object {"name":"GDT","age":"23","University":"GDUT"}
+// console.log('json1 type:',typeof(json1),json1) //json1 type:string {"name":"GDT","age":"23","University":"GDUT"}
+// console.log('json2 type:',typeof(json2),json2) //json2 type:object {"name":"GDT","age":"23","University":"GDUT"}
 
-// 1.JSON.parse()函数 [string转JSON]
-// 将JSON字符串转换为json对象
-console.log(JSON.parse(json1)) // object {"name":"GDT","age":"23","University":"GDUT"}
+// // 1.JSON.parse()函数 [string转JSON]
+// // 将JSON字符串转换为json对象
+// console.log(JSON.parse(json1)) // object {"name":"GDT","age":"23","University":"GDUT"}
 
-// 2.JSON.stringify()函数 [JSON转string]
-console.log(JSON.stringify(json2)) // string {"name":"GDT","age":"23","University":"GDUT"}
+// // 2.JSON.stringify()函数 [JSON转string]
+// console.log(JSON.stringify(json2)) // string {"name":"GDT","age":"23","University":"GDUT"}
 
-//3.eval()函数 [计算某个字符串，并执行其中的javascript表达式或要执行的语句]
-// eval(string)
-eval("x=2;y=3;document.write(x*y)") //6
+// //3.eval()函数 [计算某个字符串，并执行其中的javascript表达式或要执行的语句]
+// // eval(string)
+// eval("x=2;y=3;document.write(x*y)") //6
 
-//使用eval()函数也可以将JSON字符串解析为对象，这个功能能完成JSON.parse()的功能，但是也有不一样的地方：
-var info1 = JSON.parse(json1)
-console.log('info1',info1) //object
-var info2 = eval('('+json1+')')
-console.log('info2',info2) //object
+// //使用eval()函数也可以将JSON字符串解析为对象，这个功能能完成JSON.parse()的功能，但是也有不一样的地方：
+// var info1 = JSON.parse(json1)
+// console.log('info1',info1) //object
+// var info2 = eval('('+json1+')')
+// console.log('info2',info2) //object
 
-// eval()在string转json时需要用一对圆括号将字符串包起来：
-// 归结于eval本身的问题，由于json是以“{}”的方式来开始及结束的，在JS中，它会被当成一个语句块来处理，所以必须强制性的将它转换成一种表达式。
+// // eval()在string转json时需要用一对圆括号将字符串包起来：
+// // 归结于eval本身的问题，由于json是以“{}”的方式来开始及结束的，在JS中，它会被当成一个语句块来处理，所以必须强制性的将它转换成一种表达式。
 
-alert(eval("{}")); //undefined
-alert(eval('('+'{}'+')')); //[object Object]
+// alert(eval("{}")); //undefined
+// alert(eval('('+'{}'+')')); //[object Object]
+
+// let arr = [0,1,2,3,4,5,6,7,8,9];
+// arr.fill('Michael',2,5);
+// console.log(arr);
+//0,1,"Michael","Michael","Michael",5,6,7,8,9
+
+// let arr = ["www","Michael",".com"];
+// // for([index,val] of arr.entries()) {
+// //   console.log(index+':'+val)
+// // }
+
+// let list = arr.entries();
+// console.log(list);
+// console.log(list.next().value);
+// console.log('---------------');
+// console.log(list.next().value);
+// console.log('***************');
+// console.log(list.next().value);
+// console.log('666666666666666');
+
+function add (a,b=1) {
+  if(a === 0) {
+    //抛出异常
+    throw new Error('A is Error');
+  }
+  return a+b
+}
+console.log(add(0));
