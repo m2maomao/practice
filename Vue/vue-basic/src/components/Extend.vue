efa3b29609ab7dc2ae29678b2ffc929e78b1b91c
Michael <m2maomao@163.com>
2018-09-13 23:59:01 +0800

    fixed extend

--- Vue/vue-basic/src/components/Extend.vue
+++ Vue/vue-basic/src/components/Extend.vue
@@ -1,60 +1,105 @@
 <template>
   <div>
     <div id="todoItem"></div>
+    <apple></apple>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-
-// var AuthorURL = Vue.extend({
-//   template: "<p><a :href='authorUrl'>{{authorName}}</a></p>",
-//   data () {
-//     return {
-//       authorName: '百度',
-//       authorUrl: 'http://www.baidu.com'
-//     }
-//   }
-// })
-
-// new AuthorURL().$mount('#a')
-
+/*
 // 局部注册组件
-var TodoItem = Vue.extend({
+let TodoItem = Vue.extend({
   data: function () {
     return {
       todoData: [
-        { id: 0, text: '蔬菜' },
-        { id: 1, text: '奶酪' },
-        { id: 2, text: '随便其它什么人吃的东西' }
+        { id: 0, text: '蔬菜1' },
+        { id: 1, text: '奶酪1' },
+        { id: 2, text: '随便其它什么人吃的东西1' }
       ]
     }
   },
   template: `
-        <ul>
-            <li v-for='(d, i) in todoData' :key="i">
-                {{ d.text }}
-            </li>
-        </ul>
+    <ul>
+        <li v-for='(d, i) in todoData' :key="i">
+            {{ d.text }}
+        </li>
+    </ul>
   `
 })
 
 export default {
   name: 'Extend',
-  extend: TodoItem,
   mounted () {
     new TodoItem({
-      propsData: {
-        todoData: [
-          { id: 0, text: '蔬菜' },
-          { id: 1, text: '奶酪' },
-          { id: 2, text: '随便其他什么东西' }
-        ]
+      data () {
+        return {
+          todoData: [
+            { id: 0, text: '蔬菜2' },
+            { id: 1, text: '奶酪2' },
+            { id: 2, text: '随便其它什么人吃的东西2' }
+          ]
+        }
       }
     }).$mount('#todoItem')
   }
 
 }
+*/
+
+/*
+// 注册为全局组件
+let TodoItem = Vue.extend({
+  data: function () {
+    return {
+      todoData: [
+        { id: 0, text: '蔬菜1' },
+        { id: 1, text: '奶酪1' },
+        { id: 2, text: '随便其它什么人吃的东西1' }
+      ]
+    }
+  },
+  template: `
+    <ul>
+        <li v-for='(d, i) in todoData' :key="i">
+            {{ d.text }}
+        </li>
+    </ul>
+  `
+})
+
+Vue.component('apple', TodoItem)
+
+export default {
+
+}
+*/
+let TodoItem = Vue.extend({
+  data: function () {
+    return {
+      todoData: [
+        { id: 0, text: '蔬菜1' },
+        { id: 1, text: '奶酪1' },
+        { id: 2, text: '随便其它什么人吃的东西1' }
+      ]
+    }
+  },
+  template: `
+    <ul>
+        <li v-for='(d, i) in todoData' :key="i">
+            {{ d.text }}
+        </li>
+    </ul>
+  `
+})
+
+export default {
+  name: 'someName',
+  components: {
+    apple: TodoItem
+  }
+}
+
 </script>
 <style>
 </style>
