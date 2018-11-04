801171e04b64b535326797900112fe7e9c9e193f
michael <michael@michaeldeMacBook-Pro.local>
2018-11-04 22:19:17 +0800

    修改component

--- Vue/vue-basic/src/components/Component.vue
+++ Vue/vue-basic/src/components/Component.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <jspang here="China"></jspang>
-        <jspang here="USA"></jspang>
-        <jspang here="Tokyo"></jspang>
+        <jspang here="China" born-from="Chongqing"></jspang>
+        <jspang here="USA" born-from="Los Angeles"></jspang>
+        <jspang here="Tokyo" v-bind:born-from="bornFromLocation"></jspang>
+        <!-- 'v-bind:'可以简写成':' -->
     </div>
 </template>
 
@@ -10,10 +11,16 @@
 
 export default {
   name: '',
+  data () {
+    return {
+      bornFromLocation: '日本'
+    }
+  },
   components: {
     jspang: {
-      template: `<div style="color:red">Panda from {{here}}</div>`,
-      props: ['here']
+      template: `<div style="color:red">Panda from {{here}},now in {{bornFrom}}</div>`,
+      // 如果属性有中划线，那么用小驼峰形式代替
+      props: ['here', 'bornFrom']
     }
   }
 }
