1341cdfcc9721d2d9d0b9b1a1d91dc00326b7e1a
Michael <m2maomao@163.com>
2018-11-20 17:35:34 +0800

    add

--- Vue/vue-components/src/components/Vmodel.vue
+++ Vue/vue-components/src/components/Vmodel.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-
+    <p>Vmodel 组件</p>
+    <input type="text" v-bind:value="text" v-on:input="changeValue($event.target.value)" />
+    <h5>{{text}}</h5>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'Vmodel',
+  data () {
+    return {
+      text: '111'
+    }
+  },
+  methods: {
+    changeValue (txt) {
+      this.text = txt
+      console.log(this.text)
+    }
+  }
+}
+</script>
--- Vue/vue-components/src/router/routes.js
+++ Vue/vue-components/src/router/routes.js
@@ -3,5 +3,15 @@ export default [
     path: '/',
     name: 'HelloWorld',
     component: resolve => require(['@/components/HelloWorld'], resolve)
+  },
+  {
+    path: '/vmodel',
+    name: 'Vmodel',
+    component: resolve => require(['@/components/Vmodel'], resolve)
+  },
+  {
+    path: '/hello',
+    name: 'Hello',
+    component: resolve => require(['@/components/Hello'], resolve)
   }
 ]
