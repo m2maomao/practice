4cc48081fd551e1e0d846c490feae2c255b9c9cf
michael <michael@michaeldeMacBook-Pro.local>
2018-11-27 21:11:44 +0800

    add folder

---
+++ Vue/4.实例和内置组件/src/components/Example2.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2 id="maomao"></h2>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+
+let Michael = Vue.extend({
+  template: `
+    <div>This is International ClassRoom By {{name}}</div>
+  `,
+  data () {
+    return {
+      name: '毛毛'
+    }
+  }
+})
+
+export default {
+  name: 'Example2',
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    // 必须在mounted中挂载，以免找不到DOM报错
+    new Michael().$mount('#maomao')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
