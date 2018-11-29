22042a5a91d6baa991d111f541b4b863ed696f2a
Michael <m2maomao@163.com>
2018-11-29 17:32:17 +0800

    add nuxt 练习

--- Vue/nuxt-demo/pages/news/_id.vue
+++ Vue/nuxt-demo/pages/news/_id.vue
@@ -13,6 +13,19 @@
 export default {
   validate({params}) {
     return /^\d+$/.test(params.id)
+  },
+  data() {
+    return {
+      title: this.$route.params.title
+    }
+  },
+  head() {
+    return{
+      title: this.title,
+      meta:[
+        {hid:'description',name:'news',content:'This is a news page'}
+      ]
+    }
   }
 }
 </script>
