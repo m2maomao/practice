29a050b9159338d10f158b54052c1f9cbc15ed40
Michael <m2maomao@163.com>
2018-11-30 17:26:59 +0800

    add code

---
+++ Vue/nuxt-demo/pages/asyncData.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-cloak>
+    <h1>姓名:{{info.name}}</h1>
+    <h2>年龄:{{info.age}}</h2>
+    <h2>兴趣:{{info.interest}}</h2>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+  export default {
+    data() {
+      return {
+        name:'hello, world!',
+        info:null
+      }
+    },
+    // asyncData(){
+    //   return axios.get('https://api.myjson.com/bins/p82gq')
+    //   .then((res) => {
+    //     console.log(res)
+    //     return {info:res.data}
+    //   })
+    // },
+    async asyncData() {
+      let {data} = await axios.get('https://api.myjson.com/bins/p82gq')
+      return {info:data}
+    }
+  }
+</script>
+
+<style scoped>
+h1,h2{
+  text-indent:20px;
+  line-height: 1.5em;
+  font-size: 20px;
+}
+[v-cloak]{display:none}
+</style>
--- Vue/nuxt-demo/pages/index.vue
+++ Vue/nuxt-demo/pages/index.vue
@@ -6,27 +6,47 @@
       <li><nuxt-link :to="{name:'about'}">ABOUT</nuxt-link></li>
       <li><nuxt-link :to="{name:'news', params:{newsId:3306}}">NEWS</nuxt-link></li>
     </ul>
-  </div>
+    </div>
 </template>
 
 <script>
-// import Vue from 'vue'
-// import Vant from 'vant'
-// import 'vant/lib/index.css'
-// Vue.use(Vant)
-import { Button, Cell } from 'vant';
+import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+import $ from 'jquery'
+
+Vue.directive('focus', {
+  inserted:function(el) {
+    el.focus()
+  }
+})
 
 
+// import { Button, Cell } from 'vant';
 import AppLogo from '~/components/AppLogo.vue'
-
 export default {
   components: {
     AppLogo
+  },
+  data () {
+    return {
+      showBox:false
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    showInput () {
+      this.showBox = true
+      this.$refs.ipt.focus()
+      $('.search-ipt').trigger('click').focus()
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 .container {
   min-height: 100vh;
   display: flex;
@@ -54,6 +74,64 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+.btn-search{
+  background: #35495e;
+  color: #fff;
+  outline: none;
+  border: none;
+  border-radius:10px;
+  display: inline-block;
+  width: 120px;
+}
+h1,h2,li{
+  text-indent: 20px;
+  margin-top: 10px;
+}
+[v-cloak]{
+  display:none;
+}
+.box{
+  position: relative;
+  margin-left: 20px;
+  margin-top: 40px;
+  input{
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+  p{
+    position: absolute;
+    left: 0;
+    top: 0;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    color: #fff;
+  }
+}
+.show-box{
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  right:0;
+  bottom:0;
+  position: fixed;
+  background:rgba(0,0,0,.5);
+  z-index: 2;
+}
+.show{
+  display: block;
+}
+.hide{
+  display: none;
+}
+.search-ipt{
+  background: darkcyan;
+  margin: 20px;
+  z-index: 2;
+  border: 1px solid #35495e;
 }
 </style>
 
