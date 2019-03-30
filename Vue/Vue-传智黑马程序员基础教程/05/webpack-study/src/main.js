95aa179ee40ee4c3baed11761d5d5742a51d86fa
Michael <m2maomao@163.com>
2019-03-31 00:05:56 +0800

    webpack 设置

--- Vue/Vue-传智黑马程序员基础教程/05/webpack-study/src/main.js
+++ Vue/Vue-传智黑马程序员基础教程/05/webpack-study/src/main.js
@@ -7,8 +7,21 @@ import $ from 'jquery'
 //此种是在node中导入的方式
 // const $ = require('jquery)
 
+// 使用import语法，导入CSS样式表
+import './css/index.css'
+// 注意：webpack，默认只能打包处理 JS 类型的文件，无法处理其他的非JS类型的文件；
+// 如果要处理非JS类型的文件，我们需要手动安装一些合适的第三方 loader 加载器；
+// 1.如果想要打包处理 css 文件，需要安装 cnpm i style-loader css-loader -D
+// 2.打开 webpack.config.js 这个配置文件，在里面，新增一个配置节点，叫做 module，它是一个对象，在这个 module 对象身上，有个 rules 属性，这个 rules 属性是个 数组；这个数组中，存放了所有第三方文件的匹配和处理规则
+
+// 注意：webpack 处理第三方文件类型的过程：
+// 1.发现这个要处理的文件不是JS文件，然后就去配置文件中查找有没有对应的第三方 loader 规则；
+// 2.如果能找到对应的规则，就会调用对应的 loader 处理这种文件类型；
+// 3.在调用 loader 的时候，是从后往前调用的；
+// 4.当最后的一个 loader 调用完毕，会把处理的结果直接交给 webpack 进行打包合并，最终输出到 bundle.js 中去
+
 $(function(){
-  $('li:odd').css('backgroundColor','lightblue')
+  $('li:odd').css('backgroundColor','yellow')
   $('li:even').css('backgroundColor', function(){
     return '#' + 'D97634'
   })
@@ -19,3 +32,12 @@ $(function(){
 // 2. webpack 能够处理JS的兼容问题，把高级的浏览器不识别的语法转为低级的，浏览器能正常识别的语法。
 
 // 命令：webpack 要打包的文件路路径 打包好的输出的文件的路径
+
+// 使用 webpack-dev-server 这个工具，来实现自动打包编译的功能
+
+// 1.运行 npm i webpack-dev-server -D 把这个工具安装到项目的本地开发依赖
+// 2.安装完毕后，这个工具的用法和webpack命令的用法，完全一样
+// 3.由于我们是在项目中，本地安装的 webpack-dev-server，所以，无法把它当做我们的脚本命令，在 powershell 终端中直接运行；（只有那些安装到全局 -g 的工具，才能在终端中正常执行）
+// 4.注意：webpack-dev-server 这个工具，如果想要正常运行，要求：在本地项目中，必须安装 webpack
+// 5.webpack-dev-server 帮我们打包生成的 bundle.js文件，并没有存放到实际的物理磁盘上；而是，直接托管到了电脑内存中，所以我们在项目根目录中，根本找不到这个打包好的 bundle.js;
+// 6.我们可以认为，webpack-dev-server 把打包好的文件，以一种虚拟的形式托管到了咱们项目的根目录中，虽然我们看不到它，但是，可以认为，和 dist src node_modules 平级，有一个看不见的文件，叫做 bundle.js
--- Vue/Vue-传智黑马程序员基础教程/05/webpack-study/webpack.config.js
+++ Vue/Vue-传智黑马程序员基础教程/05/webpack-study/webpack.config.js
@@ -1,11 +1,51 @@
 const path = require('path')
-
+// 启用热更新的第2步
+const webpack = require('webpack')
+// 导入在内存中生成 HTML 页面的插件：html-webpack-plugin
+// 只要是插件都一定要放到 plugins 节点中去
+// 这个插件的两个作用：
+// 1.自动在内存中根据指定页面生成一个内存的页面
+// 2.自动，把打包好的 bundle.js 追加到页面中去
+const htmlWebpackPlugin = require('html-webpack-plugin')
 // 通过Node模块操作，向外面暴露一个配置对象
 module.exports = {
-  entry:path.join(__dirname,'./src/main.js'),//打包文件
-  output:{
-    path:path.join(__dirname,'./dist'),//打包好的文件存放地址
-    filename:'bundle.js'//打包好的文件名
+  entry: path.join(__dirname, './src/main.js'),//打包文件
+  output: {
+    path: path.join(__dirname, './dist'),//打包好的文件存放地址
+    filename: 'bundle.js'//打包好的文件名
   },
-  mode:'development' //设置mode
+  devServer: { // 这是配置 dev-server 命令参数的第二种形式，相对来说这种方式麻烦一些
+    open: true, // 自动打开浏览器
+    port: 3000, // 设置启动时候的运行端口
+    contentBase: 'src', // 指定托管的根目录
+    hot: true //启用热更新
+  },
+  resolve:{
+    extensions:[".js",".css"]
+  },
+  plugins: [ // 配置插件的节点
+    new webpack.HotModuleReplacementPlugin(), //new 一个热更新的模块对象，这是启用热更新的第3步
+    new htmlWebpackPlugin({ // 创建一个在内存中生成 HTML 页面的插件
+      template: path.join(__dirname, './src/index.html'), // 指定模板页面，将来会根据指定的页面路径，去生成内存中的页面
+      filename: 'index.html' //指定生成的页面的名称
+    })
+  ],
+  module: { // 这个节点，用于配置所有第三方模块加载器
+    rules: [ // 所有第三方模块的 匹配规则
+      {
+        test: /\.css$/,
+        use: [ // 从右至左处理
+          'style-loader', 
+          'css-loader'
+        ] // 配置处理 .css 文件的第三方loader规则
+      }
+    ]
+  },
+  mode: 'development' //设置mode
 }
+
+// 当我们在控制台，直接输入 webpack 命令的时候，webpack 做了以下几步：
+// 1.首先，webpack 发现，我们并没有通过命令的形式，给它指定入口和出口
+// 2.webpck 就会去项目的根目录中，查找一个叫做'webpack.config.js'的配置文件
+// 3.当找到配置文件后，webpack 会去解析执行这个配置文件，当解析执行完配置文件后，就得到了配置文件中导出的配置对象。
+// 4.当 webpack 拿到了配置对象后，就拿到了配置对象中指定的入口和出口，然后进行打包构建。
