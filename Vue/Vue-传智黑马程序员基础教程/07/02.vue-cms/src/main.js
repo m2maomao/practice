e37b97de8d4e638e528f17bfcc2153a293adf2eb
Michael <m2maomao@163.com>
2019-04-06 14:22:12 +0800

    basic project:init,router,tabbar,header

--- Vue/Vue-传智黑马程序员基础教程/07/02.vue-cms/src/main.js
+++ Vue/Vue-传智黑马程序员基础教程/07/02.vue-cms/src/main.js
@@ -1,2 +1,25 @@
 // 入口文件
-console.log('ok')
+import Vue from 'vue'
+// 1.1导入路由的包
+import VueRouter from 'vue-router'
+// 1.2安装路由模块
+Vue.use(VueRouter)
+// 1.3 导入自己的 router.js路由模块
+import router from './router.js'
+
+// 导入 MUI 的样式
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
+
+// 按需导入 Mint-UI 中的组件
+import {Header} from 'mint-ui'
+Vue.component(Header.name,Header)
+
+// 导入 App 根组件
+import app from './App.vue'
+
+var vm = new Vue({
+  el:'#app',
+  router, // 1.4 挂载路由对象到 VM 实例上
+  render:c => c(app)
+})
--- Vue/Vue-传智黑马程序员基础教程/07/02.vue-cms/src/router.js
+++ Vue/Vue-传智黑马程序员基础教程/07/02.vue-cms/src/router.js
@@ -1,9 +1,28 @@
 import VueRouter from 'vue-router'
 
+// 导入对应的路由组件
+
 // 3.创建路由对象
 var router = new VueRouter({
-  routes: [
-  ]
+  routes: [ // 配置路由规则
+    {
+      path: '/home',
+      component: resolve => require(['./components/tabbar/HomeContainer.vue'],resolve)
+    },
+    {
+      path: '/member',
+      component: resolve => require(['./components/tabbar/MemberContainer.vue'], resolve)
+    },
+    {
+      path: '/shopcart',
+      component: resolve => require(['./components/tabbar/ShopcartContainer.vue'], resolve)
+    },
+    {
+      path: '/search',
+      component: resolve => require(['./components/tabbar/SearchContainer.vue'], resolve)
+    }
+  ],
+  linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做：router-link-active
 })
 // 把路由对象暴露出去
 export default router
