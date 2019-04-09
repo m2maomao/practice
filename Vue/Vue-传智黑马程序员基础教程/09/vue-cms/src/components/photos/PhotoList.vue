12df0b0dd4733663c900982da1f610f4fe7d98f1
Michael <m2maomao@163.com>
2019-04-09 23:37:42 +0800

    photo list

--- Vue/Vue-传智黑马程序员基础教程/09/vue-cms/src/components/photos/PhotoList.vue
+++ Vue/Vue-传智黑马程序员基础教程/09/vue-cms/src/components/photos/PhotoList.vue
@@ -1,17 +1,58 @@
 <template>
   <div>
-    <h3>图片列表</h3>
+    <!-- 顶部滑动条区域 -->
+    <div id="slider" class="mui-slider">
+      <div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
+        <div class="mui-scroll">
+          <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
+            推荐
+          </a>
+          <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
+            热点
+          </a>
+          <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
+            北京
+          </a>
+          <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
+            社会
+          </a>
+          <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
+            娱乐
+          </a>
+          <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
+            科技
+          </a>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+// 1.导入 mui 的js文件
+import mui from '../../lib/mui/js/mui.min.js'
+// 2.初始化滑动控件
+mui('body').on('click','a',function(){document.location.href=this.href;});
+mui('.mui-scroll-wrapper').scroll({
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
 export default {
-  
+  data () {
+    return {
+
+    }
+  },
+  created() {
+    
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+* {
+  touch-action: pan-y;
+}
 </style>
 
 
