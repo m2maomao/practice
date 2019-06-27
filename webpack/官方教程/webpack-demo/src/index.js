8965a59f7870f56fb742c2f765eed528ec570f86
Michael <m2maomao@163.com>
2019-06-27 09:56:32 +0800

    webpack 懒加载

--- webpack/官方教程/webpack-demo/src/index.js
+++ webpack/官方教程/webpack-demo/src/index.js
@@ -1,16 +1,19 @@
-import {cube} from './math.js'
-
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!')
-}
+import _ from 'lodash'
 
 function component() {
-  var element = document.createElement('pre')
+  var element = document.createElement('div')
+  var button = document.createElement('button')
+  var br = document.createElement('br')
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n')
+  button.innerHTML = 'Click me and look at the console!'
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.appendChild(br)
+  element.appendChild(button)
+
+  button.onclick = e => import('./print').then(module => {
+    var print = module.default
+    print()
+  })
   
   return element
 }
--- webpack/官方教程/webpack-demo/src/print.js
+++ webpack/官方教程/webpack-demo/src/print.js
@@ -1,5 +1,5 @@
+console.log('The print.js module has loaded! See the network tab in dev tools')
+
 export default function printMe() {
-  // console.log('I get called from print.js!')
-  // cosnole.error('I get called from print.js!')
-  console.log('Updating print.js...')
+  console.log('Button Clicked: Here\'s "some text"!')
 }
