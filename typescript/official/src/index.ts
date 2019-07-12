be798b2a341d4129bc51fb0df4e80022af0cbd1b
Michael <m2maomao@163.com>
2019-07-12 17:24:46 +0800

    typescript 文件声明

--- typescript/official/src/index.ts
+++ typescript/official/src/index.ts
@@ -1,14 +1,19 @@
+/* declare var */
 jQuery('#foo');
 // 使用 declare let 定义的 jQuery 类型，允许修改这个全局变量
 // jQuery = function(selector) {
 //   return document.querySelector(selector);
 // }
-
 // 一般来说，全局变量都是禁止修改的常量，所以大部分情况都应该使用 const 而不是 var 或 let。
 // 需要注意的是，声明语句中只能定义类型，切勿在声明语句中定义具体的实现：
 
-jQuery(function() {
+/* declare function */
+jQuery(function () {
   alert('Dom Ready!');
 });
 
+/* declare class */
 var cat = new Animal('小花');
+
+/* declare enum */
+let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right];
