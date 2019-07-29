e45b5c2faaf0aa99d8b663d7224d19081952e74d
Michael <m2maomao@163.com>
2019-07-29 09:28:07 +0800

    webpack

--- webpack/webpack4/04/webpack.config.js
+++ webpack/webpack4/04/webpack.config.js
@@ -1,3 +1,37 @@
-module.exports = {
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
+module.exports = {
+  mode:'development',
+  entry:'./src/index.js',
+  module:{
+    noParse:/jquery/,// 不去解析jquery的依赖关系
+    rules:[
+      {
+        test:/\.js$/,
+        exclude:/node_modules/, // 排除
+        include:path.resolve('src'), // 包含
+        use:{
+          loader:'babel-loader',
+          options:{
+            presets:[
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  output:{
+    filename:'bundle.js',
+    path:path.resolve(__dirname,'dist')
+  },
+  plugins:[
+    new webpack.IgnorePlugin(/\.\/locale/,/moment/),
+    new htmlWebpackPlugin({
+      template:'./public/index.html'
+    })
+  ]
 }
