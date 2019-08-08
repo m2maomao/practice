901d4e4f71e757a0b4fd44541e9d3996d9162772
Michael <m2maomao@163.com>
2019-08-08 17:33:51 +0800

    react

--- React/尚硅谷/react-staging/src/App.jsx
+++ React/尚硅谷/react-staging/src/App.jsx
@@ -1,7 +1,18 @@
 import React, {Component} from 'react'
+import Add from './components/add/Add'
+import List from './components/list/List'
 
-class App extends Component {
+export default class App extends Component {
+  state = {comments:[
+    {
+      userId:'001',userName:'毛毛',content:'react is so Easy'
+    },
+    {
+      userId:'002',userName:'欣欣',content:'react is so Hard'
+    }
+  ]}
   render() {
+    let {comments} = this.state
     return (
       <div>
         <header className="site-header jumbotron">
@@ -14,43 +25,8 @@ class App extends Component {
           </div>
         </header>
         <div className="container">
-          <div className="col-md-4">
-            <form className="form-horizontal">
-              <div className="form-group">
-                <label>用户名</label>
-                <input type="text" className="form-control" placeholder="用户名" />
-              </div>
-              <div className="form-group">
-                <label>评论内容</label>
-                <textarea className="form-control" rows="6" placeholder="评论内容"></textarea>
-              </div>
-              <div className="form-group">
-                <div className="col-sm-offset-2 col-sm-10">
-                  <button type="button" className="btn btn-default pull-right">提交</button>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div className="col-md-8">
-            <h3 className="reply">评论回复：</h3>
-            <h2 style='display: none'>暂无评论，点击左侧添加评论！！！</h2>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <div className="handle">
-                  <a href="javascript:;">删除</a>
-                </div>
-                <p className="user"><span >xxx</span><span>说:</span></p>
-                <p className="centence">React不错!</p>
-              </li>
-              <li className="list-group-item">
-                <div className="handle">
-                  <a href="javascript:;">删除</a>
-                </div>
-                <p className="user"><span >yyy</span><span>说:</span></p>
-                <p className="centence">React有点难!</p>
-              </li>
-            </ul>
-          </div>
+          <Add />
+          <List comments={comments}/>
         </div>
       </div>
     )
