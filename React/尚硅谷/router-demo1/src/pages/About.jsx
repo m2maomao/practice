c785e441bedccf27771900ab537454db41f04ad9
Michael <m2maomao@163.com>
2019-08-13 16:21:48 +0800

    React

--- React/尚硅谷/router-demo1/src/pages/About.jsx
+++ React/尚硅谷/router-demo1/src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavLink, Route, Redirect} from 'react-router-dom';
 import Gallery from './Gallery'
 import Portfolio from './Portfolio'
+import '../Global.css'
 
 class About extends React.Component {
   render() {
@@ -9,8 +10,8 @@ class About extends React.Component {
       <div>
         <h3>This is About页面</h3>
         <ul>
-          <li><NavLink to="/about/gallery">Gallery</NavLink></li>
-          <li><NavLink to="/about/portfolio">Portfolio</NavLink></li>
+          <li><NavLink to="/about/gallery" className="simple-link" activeClassName="active-link">Gallery</NavLink></li>
+          <li><NavLink to="/about/portfolio" className="simple-link" activeClassName="active-link">Portfolio</NavLink></li>
         </ul>
         <Route path="/about/gallery" component={Gallery} />
         <Route path="/about/portfolio" component={Portfolio} />
