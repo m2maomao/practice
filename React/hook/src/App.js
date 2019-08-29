b049901877924aafd003aeb3d9befe7fe492bd05
Michael <m2maomao@163.com>
2019-08-29 16:04:02 +0800

    hooks

--- React/hook/src/App.js
+++ React/hook/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  // 声明一个“count”的 state 变量
+  const [count,setCount] = useState(0);
+
+  // 相当于 componentDidMount 和 componentDidUpdate
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+    console.log(22)
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => {
+        setCount(count+1)
+        console.log(11)
+      }}>Click me</button>
     </div>
   );
 }
---
+++ React/hook/src/解构赋值.js
@@ -0,0 +1,6 @@
+var foo = ["one", "two", "three"];
+
+var [a,b,c] = foo;
+console.log(a)
+console.log(b)
+console.log(c)
