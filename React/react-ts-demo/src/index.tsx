3cc43b83d4e0099adb037e3dc7a1d8b5ba0e41b2
Michael <m2maomao@163.com>
2020-01-07 15:56:48 +0800

    add redux

--- React/react-ts-demo/src/index.tsx
+++ React/react-ts-demo/src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import AppRouter from './router/AppRouter';
+import './index.css';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import store from '../store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>, 
+    document.getElementById('root')
+);
