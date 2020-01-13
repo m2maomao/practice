b8ff23d422fb1a5cc0b37d84aa418c269106d162
Michael <m2maomao@163.com>
2020-01-13 17:11:23 +0800

    安装redux-thunk

--- React/ReduxDemo/redux-demo01/src/store/index.js
+++ React/ReduxDemo/redux-demo01/src/store/index.js
@@ -1,9 +1,13 @@
-import {createStore} from 'redux' // 引入redux中createStore方法
+import {createStore, applyMiddleware, compose} from 'redux' // 引入redux中createStore方法, compose为增强函数
 import reducer from './reducer'
+import thunk from 'redux-thunk'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+const enhancer = composeEnhancers(applyMiddleware(thunk))
 
 const store = createStore(
     reducer, // 初始化store,传递reducer，reducer注入
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    enhancer
 )
 
 export default store // 导出store
