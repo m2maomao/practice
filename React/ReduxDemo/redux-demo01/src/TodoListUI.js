204b6a841f0bd681f3080055f6ee03995937189e
Michael <m2maomao@163.com>
2020-01-13 13:56:56 +0800

    无状态组件

--- React/ReduxDemo/redux-demo01/src/TodoListUI.js
+++ React/ReduxDemo/redux-demo01/src/TodoListUI.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 
-class TodoListUI extends Component {
-    render() { 
-        return ( 
-            <div style={{margin:'10px'}}>
-                <div>
-                    <Input 
-                        placeholder={this.props.inputValue}
-                        style={{width:'250px', marginRight:'10px'}}
-                        onChange={this.props.changeInputValue}
-                        value={this.props.inputValue}
-                    />
-                    <Button 
-                        type="primary"
-                        onClick={this.props.clickBtn}
-                    >Add</Button>
-                </div>
-                <div style={{marginTop:'10px', width:'310px'}}>
-                    <List
-                        bordered
-                        dataSource={this.props.list}
-                        renderItem={(item, i) => (<List.Item onClick={() => this.props.deleteItem(i)}>{item}</List.Item>)}
-                    />
-                </div>
+const TodoListUI = (props) => {
+    return ( 
+        <div style={{margin:'10px'}}>
+            <div>
+                <Input 
+                    placeholder={props.inputValue}
+                    style={{width:'250px', marginRight:'10px'}}
+                    onChange={props.changeInputValue}
+                    value={props.inputValue}
+                />
+                <Button 
+                    type="primary"
+                    onClick={props.clickBtn}
+                >Add</Button>
             </div>
-        );
-    }
+            <div style={{marginTop:'10px', width:'310px'}}>
+                <List
+                    bordered
+                    dataSource={props.list}
+                    renderItem={(item, i) => (<List.Item onClick={() => props.deleteItem(i)}>{item}</List.Item>)}
+                />
+            </div>
+        </div>
+    );
 }
  
 export default TodoListUI;
