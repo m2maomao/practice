90cbee2a609920888364f35dd37ea8ef7a0691c7
m2maomao <m2maomao@163.com>
2020-03-03 13:27:17 +0800

    基本类型，枚举类型

---
+++ typescript/geek/src/enum.ts
@@ -0,0 +1,27 @@
+enum Role {
+    Reporter = 1,
+    Developer,
+    Maintainer,
+    Owner,
+    Guest
+}
+// console.log(Role.Reporter)
+// console.log(Role[1])
+// console.log(Role);
+
+const enum Month {
+    Jan,
+    Feb,
+    Mar
+}
+let month = [Month.Jan, Month.Feb]
+console.log(month)
+
+// 枚举类型
+enum E { a, b }
+enum F { a = 1, b = 2 }
+enum G { a = 'apple', b = 'banana' }
+
+let e: E = 3
+let f: F = 3
+// e === f
--- typescript/geek/src/index.ts
+++ typescript/geek/src/index.ts
@@ -1,2 +1,5 @@
+import './datatype';
+import './enum'
+
 let hello : string = 'Hello, Typescript'
 document.querySelectorAll('.app')[0].innerHTML = hello;
