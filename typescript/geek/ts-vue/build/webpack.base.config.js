6e56ac40cf9a3989fdb2e881787dfd63980c3984
m2maomao <m2maomao@163.com>
2020-03-24 11:13:25 +0800

    搭建Vue开发环境

--- typescript/geek/ts-vue/build/webpack.base.config.js
+++ typescript/geek/ts-vue/build/webpack.base.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     entry: './src/index.ts',
@@ -6,22 +7,42 @@ module.exports = {
         filename: 'app.js'
     },
     resolve: {
-        extensions: ['.js', '.ts', '.tsx']
+        extensions: ['.js', '.ts', '.tsx', '.vue'],
+        alias: {
+            vue: 'vue/dist/vue.esm.js' // 选择es6版本
+        }
     },
     module: {
         rules: [
             {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
                 test: /\.tsx?$/i,
                 use: [{
-                    loader: 'ts-loader'
+                    loader: 'ts-loader',
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/] // 给vue添加ts扩展名
+                    }
                 }],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/tpl/index.html'
-        })
-    ]
+        }),
+        new VueLoaderPlugin()
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
 }
--- typescript/geek/ts-vue/src/index.ts
+++ typescript/geek/ts-vue/src/index.ts
@@ -1,2 +1,21 @@
-let hello: string = 'Hello TypeScript!'
-document.querySelectorAll('.app')[0].innerHTML = hello
+// let hello: string = 'Hello TypeScript!'
+// document.querySelectorAll('.app')[0].innerHTML = hello
+
+import Vue from 'vue';
+import Hello from './components/Hello.vue';
+
+// let app1 = new Vue({
+//     el: '.app',
+//     data: {
+//         name: 'TypeScript'
+//     },
+//     template: `<h1>Hello {{name}} </h1>`
+// })
+
+let app2 = new Vue({
+    el: '.app',
+    components: {
+        Hello
+    },
+    template: '<Hello />'
+})
